af9790eaa66c455a562e86a952fe7f8a71343bb3
Franklin Pazmino <franklin@pazmi.no>
2019-12-16 23:39:55 +1100

    Add hardcoded stone/pastel themes

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
 // reference: set properties
 // document.documentElement.style.setProperty('--bg-img', `url(${src})`);
 // document.documentElement.style.setProperty('--bg-main', `#232836`);
+
+const body = document.querySelector('body');
+
+document.querySelector('#stone').addEventListener('click', () => {
+  body.className = '';
+  body.classList.add('stones');
+  body.classList.add('img');
+});
+
+document.querySelector('#pastel').addEventListener('click', () => {
+  body.className = '';
+  body.classList.add('pastel');
+  body.classList.add('img');
+});
