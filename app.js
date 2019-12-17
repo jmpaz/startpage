618419c56b1e35511c28d90b95b8ebc81992cf46
Franklin Pazmino <franklin@pazmi.no>
2019-12-17 18:16:02 +1100

    Add pywal schemes

--- app.js
+++ app.js
@@ -4,18 +4,44 @@
 
 const body = document.querySelector('body');
 
+async function setTheme(theme) {
+  const getScheme = async () => {
+    const response = await fetch(`../pywal/${theme}.json`);
+    return await response.json();
+  };
+  const scheme = await getScheme().then(data => data);
+  const imgSrc = `url(images/${theme}.jpg)`;
+
+  const colors = {
+    bgMain: scheme.colors.color0,
+    bgCard: scheme.special.background,
+    bgHover: scheme.colors.color5,
+    fgMain: scheme.special.foreground,
+    fgHover: scheme.colors.color0
+  };
+
+  body.classList.add('img');
+  body.style.setProperty('background-image', imgSrc);
+  document.documentElement.style.setProperty('--bg-card', colors.bgCard);
+  document.documentElement.style.setProperty('--bg-hover', colors.bgHover);
+  document.documentElement.style.setProperty('--fg-main', colors.fgMain);
+  document.documentElement.style.setProperty('--fg-hover', colors.fgHover);
+}
+
 document.querySelector('#default').addEventListener('click', () => {
   body.className = '';
-});
-
-document.querySelector('#stone').addEventListener('click', () => {
-  body.className = '';
-  body.classList.add('stones');
-  body.classList.add('img');
+  body.style = '';
+  document.documentElement.style = '';
 });
 
 document.querySelector('#pastel').addEventListener('click', () => {
   body.className = '';
-  body.classList.add('pastel');
-  body.classList.add('img');
+  body.style = '';
+  setTheme('pastel');
+});
+
+document.querySelector('#stones').addEventListener('click', () => {
+  body.className = '';
+  body.style = '';
+  setTheme('stones');
 });
