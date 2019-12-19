632479ddca292912ad50bd61aa174fb334c45f27
Franklin Pazmino <franklin@pazmi.no>
2019-12-19 21:14:23 +1100

    Refactor app.js

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 /* global SlimSelect */
 const body = document.querySelector('body');
 
-async function setTheme(theme, isImgTheme = false) {
-  const getScheme = async () => {
-    const response = await fetch(`schemes/${theme}.json`);
-    return await response.json();
-  };
-  const scheme = await getScheme().then(data => data);
-  const [colors, special] = [scheme.colors, scheme.special];
+const getColorScheme = async theme => {
+  const response = await fetch(`schemes/${theme}.json`);
+  const data = await response.json();
+  return [data.colors, data.special];
+};
+
+const setTheme = async (theme, isImgTheme = false) => {
+  const [colors, special] = await getColorScheme(theme).then(data => data);
 
   if (isImgTheme) {
     const imgSrc = `url(images/${theme}.jpg)`;
@@ -20,44 +21,48 @@ async function setTheme(theme, isImgTheme = false) {
   document.documentElement.style.setProperty('--bg-hover', colors.color5);
   document.documentElement.style.setProperty('--fg-main', special.foreground);
   document.documentElement.style.setProperty('--fg-hover', colors.color0);
-}
+};
+
+const dropdownData = [
+  { placeholder: true, text: 'Select a theme...' },
+  { text: 'Default (system)', value: 'default' }, // regular option
+  {
+    label: 'Flat',
+    options: [
+      { class: 'flat', text: 'Ayu Mirage', value: 'ayu-mirage' },
+      { class: 'flat', text: 'Lakeside', value: 'lakeside' }
+    ]
+  },
+  {
+    label: 'Images',
+    options: [
+      { class: 'img', text: 'Mist', value: 'mist' },
+      { class: 'img', text: 'Stones', value: 'stones' },
+      { class: 'img', text: 'Pastel', value: 'pastel' }
+    ]
+  }
+];
+
+const selectionHandler = selection => {
+  body.className = '';
+  body.style = '';
+
+  switch (selection.class) {
+    case 'flat':
+      setTheme(selection.value);
+      break;
+    case 'img':
+      setTheme(selection.value, true);
+      break;
+    default:
+      document.documentElement.style = '';
+  }
+};
 
 new SlimSelect({
   select: '#theme',
+  data: dropdownData,
   showSearch: false,
   hideSelectedOption: true,
-  data: [
-    { placeholder: true, text: 'Select a theme...' },
-    { text: 'Default (system)', value: 'default' }, // regular option
-    {
-      label: 'Flat',
-      options: [
-        { class: 'flat', text: 'Ayu Mirage', value: 'ayu-mirage' },
-        { class: 'flat', text: 'Lakeside', value: 'lakeside' }
-      ]
-    },
-    {
-      label: 'Images',
-      options: [
-        { class: 'img', text: 'Mist', value: 'mist' },
-        { class: 'img', text: 'Stones', value: 'stones' },
-        { class: 'img', text: 'Pastel', value: 'pastel' }
-      ]
-    }
-  ],
-  onChange: selection => {
-    body.className = '';
-    body.style = '';
-
-    switch (selection.class) {
-      case 'flat':
-        setTheme(selection.value);
-        break;
-      case 'img':
-        setTheme(selection.value, true);
-        break;
-      default:
-        document.documentElement.style = '';
-    }
-  }
+  onChange: selectionHandler
 });
